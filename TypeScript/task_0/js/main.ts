2d031e39fa1996d16dd3c78ebdd734b5a857d84f
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-26 16:27:04 +0200

    node module package

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,29 @@
+interface Student{
+    firstName: String;
+    lastName: String;
+    age: Number;
+    location: String;
+}
+
+const student1: Student = {
+    firstName: "Josh",
+    lastName: "Dawn",
+    age: 19,
+    location: "New-York"
+}
+
+const student2: Student = {
+    firstName: "Joush",
+    lastName: "Down",
+    age: 19,
+    location: "New-York"
+}
+
+let studentList = [student1, student2]
+
+const body = document.body,
+      tbl = document.createElement('table');
+
+      for(let i in studentList) {
+        console.log(i)
+      }
