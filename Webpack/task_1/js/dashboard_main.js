c243be7ccdf89f1be8feebf6162d873e6a20b134
Matzuc2 <9793@holbertonstudents.com>
2026-07-22 11:06:16 +0200

    Add initial Webpack configuration and HTML template
    - Created webpack.config.js for bundling JavaScript files.
    - Added index.html to serve the bundled JavaScript.
    - Included LICENSE.txt for jQuery and Lodash libraries.

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,22 @@
+const $ = require('jquery');
+const _ = require('lodash');
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+  let count = 0;
+
+  $('button').on(
+    'click',
+    _.debounce(() => {
+      count += 1;
+      $('#count').text(`${count} clicks on the button`);
+    }, 500),
+  );
+}
+
+updateCounter();
