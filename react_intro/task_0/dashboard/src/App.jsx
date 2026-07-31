b09e3c51a7a4cf01f5332c027b058c708c0419dc
Matzuc2 <9793@holbertonstudents.com>
2026-07-31 15:32:19 +0200

    checker

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,29 +1,21 @@
-import closeButton from '../public/close-button.png'
-import favicon from '../public/favicon.ico'
 import holbertonLogo from '../public/holberton-logo.jpg'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <div className='App-header'>
-        <header>
-          <img src={holbertonLogo} alt='holberton logo' />
-          <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
-        </header>
-      </div>
-      <div className='App-body'>
-        <body>
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
-        </body>
       </div>
-      <div className='App-footer'>
-        <footer>
-          <p>Copyright 2026 - Holberton School</p>
-        </footer>
-      </div>
-    </>
-  )
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
