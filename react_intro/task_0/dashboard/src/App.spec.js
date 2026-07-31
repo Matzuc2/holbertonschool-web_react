63d4c345cc264e3c217f116bb358beb00a7f13ce
Matzuc2 <9793@holbertonstudents.com>
2026-07-31 17:20:28 +0200

    task 1, all files for testing

---
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -0,0 +1,23 @@
+import {render, screen} from '@testing-library/react'
+import App from './App'
+
+
+test('renders School Dashboard h1', () => {
+  render(<App />)
+  const h1 = screen.getByRole('heading', { name: /School dashboard/i })
+  expect(h1).toBeInTheDocument()
+})
+test('renders School Dashboard p', () =>{
+  render(<App/>)
+  const pLogin = screen.getByText(/Login to access the full dashboard/i)
+  const pCopyright = screen.getByText(/copyright - Holberton School/i)
+
+  expect(pLogin).toBeInTheDocument()
+  expect(pCopyright).toBeInTheDocument()
+})
+
+test('renders School Dashboard img', () =>{
+    render(<App/>)
+    const image = screen.getByAltText(/Holberton logo/i)
+    expect(image).toBeInTheDocument()
+})
