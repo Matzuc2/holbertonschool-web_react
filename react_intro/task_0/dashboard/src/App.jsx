5eea52f6fe20e842257d8f16d97890bd656596cf
Matzuc2 <9793@holbertonstudents.com>
2026-07-31 15:00:10 +0200

    Adding all necessary files to task_0

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,23 @@
+import closeButton from '../public/close-button.png'
+import favicon from '../public/favicon.ico'
+import holbertonLogo from '../public/holberton-logo.jpg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='App-header'>
+        <img src={holbertonLogo} alt='holberton logo' />
+        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
+      </div>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className='App-footer'>
+        <p>Copyright {new Date().getFullYear()} - holberton School</p>
+      </div>
+    </>
+  )
+}
+
+export default App
