8cbdffaddce113bcd655b72d8c24a5f420a45967
Matzuc2 <9793@holbertonstudents.com>
2026-07-31 15:20:26 +0200

    change

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -7,14 +7,20 @@ function App() {
   return (
     <>
       <div className='App-header'>
-        <img src={holbertonLogo} alt='holberton logo' />
-        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
+        <header>
+          <img src={holbertonLogo} alt='Holberton logo' />
+          <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
+        </header>
       </div>
       <div className='App-body'>
+        <body>
         <p>Login to access the full dashboard</p>
+        </body>
       </div>
       <div className='App-footer'>
-        <p>Copyright {new Date().getFullYear()} - Holberton School</p>
+        <header>
+          <p>Copyright {new Date().getFullYear()} - Holberton School</p>
+        </header>
       </div>
     </>
   )
