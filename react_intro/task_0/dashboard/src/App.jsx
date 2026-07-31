10ad8b5d387d255b75dad2c1c7a4b1ea3da1a228
Matzuc2 <9793@holbertonstudents.com>
2026-07-31 15:35:05 +0200

    checker

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <p>Login to access the full dashboard</p>
       </div>
       <footer className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+        <p>copyright - Holberton School</p>
       </footer>
     </div>
   );
