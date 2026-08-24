12f40140af72464db24d0b8a95cbbf13e01720a4
Matzuc2 <9793@holbertonstudents.com>
2026-08-24 16:12:20 +0200

    fix: correct import path for Notifications component and clean up App.spec.js tests

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -2,7 +2,7 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import './App.css'
-import Notifications from './Notifications'
+import Notifications from '../Notifications/Notifications'
 
 function App() {
   return (
--- react_props/task_0/dashboard/src/App/App.spec.js
+++ react_props/task_0/dashboard/src/App/App.spec.js
@@ -1,48 +1,9 @@
+
 import {render, screen} from '@testing-library/react'
 import App from './App'
 
-
-test('renders School Dashboard h1', () => {
-  render(<App />)
-  const h1 = screen.getByRole('heading', { name: /School dashboard/i })
-  expect(h1).toBeInTheDocument()
-})
-
-
-test('renders School Dashboard p', () => {
-  render(<App />)
-  const pLogin = screen.getByText(/Login to access the full dashboard/i)
-  const pCopyright = screen.getByText(/holberton School/i)
-
-  expect(pLogin).toBeInTheDocument()
-  expect(pCopyright).toBeInTheDocument()
-})
-
-test('renders School Dashboard img', () =>{
-    render(<App/>)
-    const image = screen.getByAltText(/Holberton logo/i)
-    expect(image).toBeInTheDocument()
-})
-
-
-test('renders School Dashboard inputs', () => {
+test('renders Footer', () => {
   render(<App />)
 
-  const emailInput = screen.getByLabelText(/email/i)
-  const passwordInput = screen.getByLabelText(/password/i)
-  expect(emailInput).toBeInTheDocument()
-  expect(passwordInput).toBeInTheDocument()
-})
-
-
-test('renders School Dashboard labels', () => {
-  render(<App />)
-  expect(screen.getByText(/email/i)).toBeInTheDocument()
-  expect(screen.getByText(/password/i)).toBeInTheDocument()
-})
-
-
-test('renders School Dashboard button', () => {
-  render(<App />)
-  expect(screen.getByRole('button', { name: /OK/i })).toBeInTheDocument()
+  expect(screen.getByRole('contentinfo')).toBeInTheDocument()
 })
