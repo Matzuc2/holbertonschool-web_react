86c7b22b919d4220ae8badbf8b13a9cc775679bb
Matzuc2 <9793@holbertonstudents.com>
2026-08-24 18:27:36 +0200

    feat: add tests for Login component and implement focus behavior on label clicks

--- react_props/task_1/dashboard/src/Login/Login.jsx
+++ react_props/task_1/dashboard/src/Login/Login.jsx
@@ -1,13 +1,13 @@
 function Login(){
     return(
-    <div className="Login">
-        <p>Login to access the full dashboard</p>
-        <label htmlFor="email">email:</label>
-        <input type="text" id="email" name="email"></input>
-        <label htmlFor="password">password:</label>
-        <input type="text" id="password" name="password"></input>
-        <button>OK</button>
-    </div>
+        <div className="Login">
+            <p>Login to access the full dashboard</p>
+            <label htmlFor="email" onClick={() => document.getElementById('email').focus()}>email:</label>
+            <input type="text" id="email" name="email" />
+            <label htmlFor="password" onClick={() => document.getElementById('password').focus()}>password:</label>
+            <input type="text" id="password" name="password" />
+            <button>OK</button>
+        </div>
     )
 }
 export default Login;
