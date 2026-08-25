da8ca690c5a34074a3ee6e000ac5299370e703dc
Matzuc2 <9793@holbertonstudents.com>
2026-08-25 15:39:50 +0200

    fix: simplify color assertion in NotificationItem tests by removing unnecessary style retrieval

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.spec.js
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.spec.js
@@ -4,13 +4,11 @@ import NotificationItem from './NotificationItem'
 test('renders good color depending on default type', () => {
   render(<NotificationItem  type='default' />)
   const li = document.querySelector('li')
-  const styles = window.getComputedStyle(li)
-  expect(styles.color).toBe('blue')
+  expect(li.style.color).toBe('blue')
 })
 
 test('renders good color depending on urgent type', () => {
   render(<NotificationItem  type='urgent' />)
   const li = document.querySelector('li')
-  const styles = window.getComputedStyle(li)
-  expect(styles.color).toBe('red')
+  expect(li.style.color).toBe('red')
 })
