8124f26b6be8b3a7966e4b5e1fc8534a03524034
Matzuc2 <9793@holbertonstudents.com>
2026-08-25 16:09:42 +0200

    feat: initialize dashboard project with React, Vite, and testing setup
    - Add package.json with dependencies and scripts for development and testing
    - Create initial project structure including App, Header, Footer, Login, and Notifications components
    - Implement styles for components and global styles
    - Set up Jest testing framework with initial tests for components
    - Add utility functions for footer and notification handling
    - Include favicon and logo assets
    - Configure Vite for module bundling

---
+++ react_props/task_3/dashboard/src/App/App.jsx
@@ -0,0 +1,27 @@
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import './App.css'
+import Notifications from '../Notifications/Notifications'
+
+function App() {
+  const notificationsList = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', value: 'New data available' },
+  ]
+  return (
+    <>
+      <div className="App">
+          <div className='root-notifications'>
+            <Notifications notifications={notificationsList}/>
+          </div>
+          <Header/>
+          <Login/>
+          <Footer/>
+      </div>
+    </>
+  );
+}
+
+export default App;
