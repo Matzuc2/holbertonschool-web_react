de3fab290a01999c6ce41ee7b60b15934fae4003
Matzuc2 <9793@holbertonstudents.com>
2026-08-26 18:39:38 +0200

    fix: refactor CourseList component structure for improved readability

--- react_props/task_4/dashboard/src/CourseList/CourseList.jsx
+++ react_props/task_4/dashboard/src/CourseList/CourseList.jsx
@@ -3,19 +3,26 @@ import CourseListRow from "./CourseListRow"
 function CourseList({courses = []}){
     return(
         <table id="CourseList">
-            <thead>
-                <CourseListRow textFirstCell="Available courses" isHeader={true}/>
-                <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true}/>
-            </thead>
-            <tbody>
-                {courses[0] ?
-                    courses.map((course)=>(
-                        <CourseListRow key={course.id} textFirstCell={course.name} textSecondCell={course.credit}/>)
-                    ):
+            {courses.length > 0 ?
+            <>
+                <thead>
+                    <CourseListRow textFirstCell="Available courses" isHeader={true}/>
+                    <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true}/>
+                </thead>
+                <tbody>
+                    {
+                        courses.map((course)=>(
+                            <CourseListRow key={course.id} textFirstCell={course.name} textSecondCell={course.credit}/>)
+                        )
+                        
+                    }
+                </tbody>
+            </>
+                :
+                <thead>
                     <CourseListRow textFirstCell="No course available yet"isHeader={true}/>
-                    
+                </thead>
                 }
-            </tbody>
         </table>
     )
 }
