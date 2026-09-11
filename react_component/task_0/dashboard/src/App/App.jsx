1db8d40ebc796c40a06f8cf84ef42b83671022b1
Matzuc2 <9793@holbertonstudents.com>
2026-09-11 10:35:59 +0200

    feat: implement App component structure with Header, Login, Footer, and Notifications

--- react_component/task_0/dashboard/src/App/App.jsx
+++ react_component/task_0/dashboard/src/App/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import './App.css'
+import Notifications from '../Notifications/Notifications'
 
-class App extends React.Component{
-    render(){
-        return <></>
-    }
+class App extends React.Component() {
+  render() {
+    return(
+    <>
+      <div className="App">
+          <div className='root-notifications'>
+            <Notifications/>
+          </div>
+          <Header/>
+          <Login/>
+          <Footer/>
+      </div>
+    </>)
+  };
 }
+
+export default App;
